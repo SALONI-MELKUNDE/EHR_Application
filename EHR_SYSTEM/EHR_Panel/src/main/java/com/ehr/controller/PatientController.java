587d443b913fe1ac9d7ab4c9dcdae2b15e819c00
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-09 19:21:32 +0100

    upadted controller

--- EHR_SYSTEM/EHR_Panel/src/main/java/com/ehr/controller/PatientController.java
+++ EHR_SYSTEM/EHR_Panel/src/main/java/com/ehr/controller/PatientController.java
@@ -27,38 +27,38 @@ public class PatientController {
 
 	@PostMapping("/addPatients")
 	public ResponseEntity<Map<String, Object>> addPatient(@RequestBody Patient patient) {
-	    Patient savedPatient = patientService.savePatient(patient);
+		Patient savedPatient = patientService.savePatient(patient);
 
-	    Map<String, Object> response = new HashMap<>();
-	    response.put("message", "Patient registered successfully");
-	    response.put("patient", savedPatient);
+		Map<String, Object> response = new HashMap<>();
+		response.put("message", "Patient registered successfully");
+		response.put("patient", savedPatient);
 
-	    return ResponseEntity.ok(response);
+		return ResponseEntity.ok(response);
 	}
 
 	@PostMapping("/addDoctors")
 	public ResponseEntity<Map<String, Object>> addDoctor(@RequestBody Doctor doctor) {
-	    Doctor savedDoctor = patientService.saveDoctor(doctor);
+		Doctor savedDoctor = patientService.saveDoctor(doctor);
 
-	    Map<String, Object> response = new HashMap<>();
-	    response.put("message", "Doctor registered successfully");
-	    response.put("doctor", savedDoctor);
+		Map<String, Object> response = new HashMap<>();
+		response.put("message", "Doctor registered successfully");
+		response.put("doctor", savedDoctor);
 
-	    return ResponseEntity.ok(response);
+		return ResponseEntity.ok(response);
 	}
 
-	   @GetMapping("/getAllDoctorRecords")
-	   public List<Doctor> getAllDoctorRecords() {
-	        return patientService.findAll();
-	    }
+	@GetMapping("/getAllDoctorRecords")
+	public List<Doctor> getAllDoctorRecords() {
+		return patientService.findAll();
+	}
 
-	    @GetMapping("/doctor/{doctorId}")
-	    public List<Doctor> getDoctorByDoctorId(@PathVariable Long doctorId) {
+	@GetMapping("/doctor/{doctorId}")
+	public List<Doctor> getDoctorByDoctorId(@PathVariable Long doctorId) {
 
-	    	  System.out.println("Received doctorId: " + doctorId);
+		System.out.println("Received doctorId: " + doctorId);
 
-	    	return patientService.findByDoctorId(doctorId);
-	    }
+		return patientService.findByDoctorId(doctorId);
+	}
 
 
 
@@ -67,23 +67,23 @@ public class PatientController {
 	public ResponseEntity<Map<String, Object>> SelfVitalsRecords(@RequestBody SelfVitalsRecords selfVitalsRecords) {
 		SelfVitalsRecords savedselfVitalsRecords = patientService.selfVitalsRecords(selfVitalsRecords);
 
-	    Map<String, Object> response = new HashMap<>();
-	    response.put("message", "selfVitalsRecords registered successfully");
-	    response.put("selfVitalsRecords", savedselfVitalsRecords);
+		Map<String, Object> response = new HashMap<>();
+		response.put("message", "selfVitalsRecords registered successfully");
+		response.put("selfVitalsRecords", savedselfVitalsRecords);
 
-	    return ResponseEntity.ok(response);
+		return ResponseEntity.ok(response);
 	}
 
-	   @GetMapping("/getAllPatients")
-	    public List<Patient> getAllPatients() {
-	        return patientService.getAllPatients();
-	    }
-	PatientController.java
-	    // Get patient by ID
-	   @GetMapping("/patient/{patientId}")
-	    public List<Patient> getPatientById(@PathVariable Long patientId) {
-	        return patientService.findByPatientId(patientId);
-	    }
+	@GetMapping("/getAllPatients")
+	public List<Patient> getAllPatients() {
+		return patientService.getAllPatients();
+	}
+
+	// Get patient by ID
+	@GetMapping("/patient/{patientId}")
+	public List<Patient> getPatientById(@PathVariable Long patientId) {
+		return patientService.findByPatientId(patientId);
+	}
 
 
 }
