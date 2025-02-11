c1eb0ad72ab83af196ae7a7468dc296113164128
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-11 19:12:36 +0100

    order of @Autowired fields was slightly changed

--- EHR_SYSTEM/EHR_Panel/src/main/java/com/ehr/service/PatientService.java
+++ EHR_SYSTEM/EHR_Panel/src/main/java/com/ehr/service/PatientService.java
@@ -1,37 +1,40 @@
 package com.ehr.service;
 
-import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 
 import com.ehr.entity.Doctor;
 import com.ehr.entity.Patient;
 import com.ehr.entity.SelfVitalsRecords;
 
+
 public interface PatientService {
-	
 
 
-    Patient savePatient(Patient patient);
+	Patient savePatient(Patient patient);
 
 	Doctor saveDoctor(Doctor doctor);
-	
-	
-	
-	
 
 
-	    SelfVitalsRecords selfVitalsRecords(SelfVitalsRecords selfVitalsRecords);
 
-		List<Doctor> findAll();
 
-		List<Doctor> findByDoctorId(Long doctorId);
+	SelfVitalsRecords selfVitalsRecords(SelfVitalsRecords selfVitalsRecords);
 
-		List<Patient> getAllPatients();
+	List<Doctor> findAll();
 
-		List<Patient> findByPatientId(Long patientId);
-	
+	List<Doctor> findByDoctorId(Long doctorId);
+
+	List<Patient> getAllPatients();
+
+	List<Patient> findByPatientId(Long patientId);
+
+
+
+
+
+
+}
 
 	
 
