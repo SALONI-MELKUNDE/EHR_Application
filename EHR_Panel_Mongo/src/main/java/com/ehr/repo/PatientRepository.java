d5b93fcc55260827f73c8581b82f2884d27e1247
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-13 16:10:02 +0100

    added patientRepository interface for MongoDB with a query to find patients by ID

---
+++ EHR_Panel_Mongo/src/main/java/com/ehr/repo/PatientRepository.java
@@ -0,0 +1,19 @@
+package com.ehr.repo;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ehr.entity.Patient;
+
+import java.util.List;
+
+@Repository  // ✅ Required to mark this as a Spring-managed repository
+public interface PatientRepository extends MongoRepository<Patient, Long> {
+
+    @Query("{ 'patient_id' : ?0 }")
+    List<Patient> findByPatientId(String patient_id);
+}
+
+
