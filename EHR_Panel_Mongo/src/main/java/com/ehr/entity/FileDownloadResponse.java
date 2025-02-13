7019183963f91cfa93092f0a27a8341839d0a726
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-13 16:02:29 +0100

    addeding FileDownloadResponse class to store file data and a message for download responses

---
+++ EHR_Panel_Mongo/src/main/java/com/ehr/entity/FileDownloadResponse.java
@@ -0,0 +1,29 @@
+package com.ehr.entity;
+
+public class FileDownloadResponse {
+    private String message;
+    private byte[] fileData;
+
+    public FileDownloadResponse(String message, byte[] fileData) {
+        this.message = message;
+        this.fileData = fileData;
+    }
+
+    // Getters and Setters
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
+}
+
