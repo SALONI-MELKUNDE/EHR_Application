dc50a75c82589614117a5c1879224c1cbee3a19a
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-13 16:15:13 +0100

    main file resolved

--- EHR_Panel_Mongo/src/main/java/com/ehr/service/PatientService.java
+++ EHR_Panel_Mongo/src/main/java/com/ehr/service/PatientService.java
@@ -8,7 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 public interface PatientService {
-	
+
 
 
 
--- EHR_Panel_Mongo/src/main/java/com/ehr/service/PatientServiceImpl.java
+++ EHR_Panel_Mongo/src/main/java/com/ehr/service/PatientServiceImpl.java
@@ -51,7 +51,6 @@ public class PatientServiceImpl implements PatientService {
 
 
 
-
 	
 
 
