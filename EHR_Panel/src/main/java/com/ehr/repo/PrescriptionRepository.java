eaa2ab7b01d71ff8786a9eff9851b3e64ef703e5
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-13 23:06:04 +0100

    implemented findByPatientIdAndMedicineName to retrieve prescription details for a specific patient and medicine

---
+++ EHR_Panel/src/main/java/com/ehr/repo/PrescriptionRepository.java
@@ -0,0 +1,16 @@
+package com.ehr.repo;
+
+import com.ehr.entity.Prescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository  // ✅ Required to mark this as a Spring-managed repository
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+
+
+    Prescription findByPatientIdAndMedicineName(Long patientId, String medicineName);
+}
+
+
