ca7af86e6e8fab3feea6f1f89f5c464f818b790d
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-27 15:06:55 +0100

    add Spring configuration annotations to Security class

--- EHR_Panel/src/main/java/com/ehr/entity/Security.java
+++ EHR_Panel/src/main/java/com/ehr/entity/Security.java
@@ -1,5 +1,11 @@
 package com.ehr.entity;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.Security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
 public class Security {
 
 }
