4a2d95130e4a22e2cce517bfc42c35a8c7173eeb
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-02-27 15:34:14 +0100

    adding userDetailsService (admin, doctor, patient)

--- EHR_Panel/src/main/java/com/ehr/entity/Security.java
+++ EHR_Panel/src/main/java/com/ehr/entity/Security.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +30,34 @@ public class Security {
                         .requestMatchers("/addDoctors").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> {}); // Basic Auth
+                .httpBasic(httpBasic -> {
+                });
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin123")  // We'll encode later
+                .roles("ADMIN")
+                .build();
+
+        UserDetails doctor = User.builder()
+                .username("doctor1")
+                .password("doctor123") // We'll encode later
+                .roles("DOCTOR")
+                .build();
+
+        UserDetails patient = User.builder()
+                .username("patient1")
+                .password("patient123") // We'll encode later
+                .roles("PATIENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, doctor, patient);
+    }
+
 }
+
