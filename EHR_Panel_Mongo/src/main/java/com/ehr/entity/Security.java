9cdf595de0f3da386f84e9a0d7f3107e255596e9
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-03-01 13:36:24 +0100

    added security dependacies and security file for Non-structured data

---
+++ EHR_Panel_Mongo/src/main/java/com/ehr/entity/Security.java
@@ -0,0 +1,60 @@
+package com.ehr.entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/upload").hasAnyRole("ADMIN", "PATIENT")
+
+                        .requestMatchers("/download/**").hasAnyRole("ADMIN", "PATIENT")
+
+                        .anyRequest().permitAll()
+                )
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+
+        UserDetails patient = User.builder()
+                .username("patient1")
+                .password(passwordEncoder().encode("patient123"))
+                .roles("PATIENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, patient);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
