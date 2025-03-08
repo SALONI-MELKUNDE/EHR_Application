80eff3efe7f4f0b34685f8287ea140bfb260a7bf
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-03-08 21:30:20 +0100

    update generic type from String to Long to match appointment entity

--- EHR_Panel/src/main/java/com/ehr/repo/AppointmentRepository.java
+++ EHR_Panel/src/main/java/com/ehr/repo/AppointmentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, String> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+
 }
