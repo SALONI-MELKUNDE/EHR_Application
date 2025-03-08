2da5dfc583bb592d4f40ff4784a1fcd35fd30742
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2025-03-08 22:55:44 +0100

    adding deleteByPatientId method for patient-based deletion

--- EHR_Panel/src/main/java/com/ehr/repo/PrescriptionRepository.java
+++ EHR_Panel/src/main/java/com/ehr/repo/PrescriptionRepository.java
@@ -1,10 +1,12 @@
 package com.ehr.repo;
 
 import com.ehr.entity.Prescription;
+import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository  // ✅ Required to mark this as a Spring-managed repository
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
@@ -14,10 +16,14 @@ public interface PrescriptionRepository extends JpaRepository<Prescription, Long
 
 
 
+
+
+
+    void deleteByPatientId(Long patientId);
+
     List<Prescription> findByPatientId(Long patientId);
+
 }
-
-
 
 
 
